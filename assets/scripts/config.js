82354a6adf27f04cced991ba62bb042c2e1dfc9b
TheLog <lgscustomww@gmail.com>
2020-08-10 14:33:28 -0500

    changed api url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://bobs-bites-api.herokuapp.com',
+  production: 'https://bobs-bites-api.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
